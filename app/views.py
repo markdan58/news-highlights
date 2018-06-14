e5e198978da3d0ed5ca9f0f16814f9df699c009f
maxwellharon <maxwellharon@gmail.com>
2018-06-14 12:54:55 +0300

    Begin

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'News'
+    title = 'Home - Welcome to the world\'s number one news site'
+    return render_template('index.html',title = title)
+
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = news_id)
