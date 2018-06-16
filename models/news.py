e9f1309f6e2e7418e081bbb273e63e291a593fc1
maxwellharon <maxwellharon@gmail.com>
2018-06-17 00:37:38 +0300

    Did index.html file

---
+++ models/news.py
@@ -0,0 +1,9 @@
+class News:
+    '''
+    highlight class to define objects
+    '''
+
+    def __init__(self, id, name, category):
+        self.id = id
+        self.name = name
+        self.category = category
